f5acfe971e5450601bfb351aae3ca0d9c75644f0
Quentin Smith <quentin@mit.edu>
2023-12-01 17:43:08 -0500

    Expose flake overlay

--- flake.nix
+++ flake.nix
@@ -3,22 +3,30 @@
 
   inputs.flake-utils.url = "github:numtide/flake-utils";
 
-  outputs = { self, nixpkgs, flake-utils }:
-    flake-utils.lib.simpleFlake {
-      inherit self nixpkgs;
-      name = "py-profinet";
-      overlay = (final: prev: {
-        pythonPackagesExtensions = prev.pythonPackagesExtensions ++ [(python-final: python-prev: with python-final; {
-          scapy = python-prev.scapy.overrideAttrs (old: {
-            patches = (old.patches or []) ++ (lib.optionals stdenv.isDarwin [
-              ./scapy/darwin-ioctl.patch
-            ]);
-          });
-        })];
-        py-profinet = {
-          py-profinet = final.callPackage ./default.nix {};
+  outputs = { self, nixpkgs, flake-utils }: let
+    overlay = (final: prev: {
+      pythonPackagesExtensions = prev.pythonPackagesExtensions ++ [(python-final: python-prev: with python-final; {
+        scapy = python-prev.scapy.overrideAttrs (old: {
+          patches = (old.patches or []) ++ (lib.optionals stdenv.isDarwin [
+            ./scapy/darwin-ioctl.patch
+          ]);
+        });
+      })];
+      py-profinet = final.callPackage ./default.nix {};
+    });
+  in
+    (flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+          overlays = [ overlay ];
         };
-      });
-      #shell = ./shell.nix;
-    };
+      in {
+        packages = rec {
+          inherit (pkgs) py-profinet;
+          default = py-profinet;
+        };
+      })) // {
+        overlays.default = overlay;
+      };
 }
